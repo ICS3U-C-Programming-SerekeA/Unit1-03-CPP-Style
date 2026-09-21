a0bb57511ef5f5972513500362f55a07e3d41243
Sereke Asfeday <sereke.asfeday@stu.ocsb.ca>
2026-09-21 01:47:59 +0000

    First commit of the the helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2026 Sereke Asfeday All rights reserved.
+// .
+// Created by: Sereke Asfeday
+// Date: Sep, 20th, 2026
+// This program displays a greeting with proper style.
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Sereke!" << std::endl;
+    std::cout << "This class is so much fun!\n";
+    std::cout << "I can't wait to learn more!" << std::endl;
+}
